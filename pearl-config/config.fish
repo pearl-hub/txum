921fe0944fd8c4c2574fd511c74a63335964a428
Filippo Squillace <squillace.filippo@gmail.com>
2020-12-16 11:46:15 +0100

    Remove PATH update

--- pearl-config/config.fish
+++ pearl-config/config.fish
@@ -1,7 +1,3 @@
 set -x TXUM_VARDIR $PEARL_PKGVARDIR
 
-if not contains $TXUM_VARDIR/bin $PATH
-    set -x PATH $PATH $TXUM_VARDIR/bin
-end
-
 # vim: ft=sh
--- pearl-config/config.sh
+++ pearl-config/config.sh
@@ -1,7 +1,2 @@
 export TXUM_VARDIR="$PEARL_PKGVARDIR"
 
-
-if [[ $PATH != *"${TXUM_VARDIR}/bin"* ]]
-then
-    export PATH="$PATH:$TXUM_VARDIR/bin"
-fi
